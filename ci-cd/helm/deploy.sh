0d9a91b4decc0df3e8387b3d58bff7ef90ee09bb
Emmanuel Ogar <84130927+emmanuelogar@users.noreply.github.com>
2025-06-18 20:09:56 +0000

    updated

--- ci-cd/helm/deploy.sh
+++ ci-cd/helm/deploy.sh
@@ -3,8 +3,8 @@
 set -e  # Exit on error
 set +x  # Don't print commands
 
-helm template ./sample-api \
-    | kubectl apply -f -
+helm template ./sample-api -f ../../.devcontainer/dev-values.yaml \
+    | kubectl apply -f - 
 
-kubectl apply -f ../../static/deployment.yaml
-kubectl apply -f ../../static/service.yaml
+# kubectl apply -f ../../static/deployment.yaml
+# kubectl apply -f ../../static/service.yaml
